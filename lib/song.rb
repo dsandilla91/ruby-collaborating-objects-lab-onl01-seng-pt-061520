83f2f2f5b6d83f8bc407cd5ed820dba5cb969ab5
Deborah Santillan <s.debb@yahoo.com>
2020-07-18 20:52:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,10 +5,7 @@ class Song
     @name = name
   end
   
-  def self.find(name)     #class method detects instances from @@all class variable
-    self.all.detect { |song| song.name == name }
-  end
-
+  
   def self.new_by_filename(file)
     artist_name = file.split(" - ")[0]
     song_name = file.split(" - ")[1]
